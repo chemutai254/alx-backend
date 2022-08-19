8376c4525cdb31928360bd2a119f0e3a643bb245
chemutai254 <nancy.chemutai254>
2022-08-19 02:44:20 -0700

    Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -3,5 +3,7 @@
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    page_index = ((page - 1) * page_size, page * page_size)
-    return page_index
+    """returns the start ad end of the index according to the range"""
+    start_index = (page - 1) * page_size
+    end_index = start_index + page_size
+    return (start_index, end_index)

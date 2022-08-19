fa4806c14eeef88caa60467030d36e3cee224d60
chemutai254 <nancy.chemutai254>
2022-08-19 02:38:14 -0700

    Simple helper function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -2,5 +2,6 @@
 """Simple helper function"""
 
 
-def index_range(page, page_size):
-    tuple.page(page_size, start_index, end_index)
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    page_index = ((page - 1) * page_size, page * page_size)
+    return page_index

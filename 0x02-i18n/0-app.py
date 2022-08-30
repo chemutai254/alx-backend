ddead688a99769bd6496069579acee4b57bfdb8b
chemutai254 <nancy.chemutai254>
2022-08-30 03:12:32 -0700

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -6,4 +6,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """Routes"""
     return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port='5000', host='0.0.0.0')

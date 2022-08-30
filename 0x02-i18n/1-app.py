f93a6ed2143ac3635436a81919a847da3b88dbb3
chemutai254 <nancy.chemutai254>
2022-08-30 01:33:08 -0700

    Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,3 +1,24 @@
 #!/usr/bin/env python3
-""""""
+"""Basic Babel setup"""
+from flask import Flask, render_template
+from flask_babel import Babel
+from app import app
 
+
+class Config(object):
+    """Use class config for the app"""
+    LANGUAGES = ['en', 'fr']
+    """sets local to english and timezone to UTC"""
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+    app.config.from_object(Config)
+    babel = Babel(app)
+
+
+    @app.route('/')
+    @app.route('/index')
+    def index():
+        """Use routes"""
+        return render_template("1-index.html")

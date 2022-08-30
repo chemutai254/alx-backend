b48ee470a59a18ed69f01b1dffc5326e8b583a9e
chemutai254 <nancy.chemutai254>
2022-08-30 03:15:38 -0700

    Basic Babel setup

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -2,7 +2,7 @@
 """Basic Babel setup"""
 from flask import Flask, render_template
 from flask_babel import Babel
-from app import app
+app = Flask(__name__)
 
 
 class Config(object):
@@ -20,3 +20,6 @@ class Config(object):
     def index():
         """Use routes"""
         return render_template("1-index.html")
+
+    if __name__ == '__main__':
+        app.run(port='5000', host='0.0.0.0')

b62b8d68c168243ede10e64172f11630c2f2cb6a
chemutai254 <nancy.chemutai254>
2022-08-30 00:09:02 -0700

    Basic Flask app

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python3
 """Creates a single route and an index html template"""
+from flask import Flask, render_template
 from app import app
 
+
 @app.route('/')
-@app.route('/index')
 def index():
-    return "Hello, World!"
+    return render_template('0-index.html')

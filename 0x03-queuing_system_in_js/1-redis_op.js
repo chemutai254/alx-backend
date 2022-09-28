a18584da37dc7db9c707578fb1c85a77c07f7ca9
chemutai254 <nancychemutai254@gmail.com>
2022-09-28 17:52:09 +0000

    Node Redis client and basic operations

---
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -0,0 +1,30 @@
+import { createClient } from 'redis';
+
+function redisConnect() {
+  const client = createClient();
+
+  client.on('connect', function() {
+    console.log('Redis client connected to the server');
+  }).on('error', (err) => {
+    console.log(`Redis client not connected to the server: ${err}`);
+  });
+
+};
+
+function setNewSchool(schoolName, value) {
+    client.set(schoolName, value, print);
+};
+
+function displaySchoolValue(schoolName) {
+    client.get(schoolName, function(error, result) {
+    if (error) {
+      console.log(error);
+      throw error;
+    }
+    console.log(result);
+  });
+};
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');

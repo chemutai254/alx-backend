44283204dc1ce0eafdc7b5655eed7ad69690fca7
chemutai254 <nancychemutai254@gmail.com>
2022-09-28 18:56:37 +0000

    Node Redis client and advanced operations

---
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -0,0 +1,8 @@
+hset key HolbertonSchools Portland (integer) 50 
+hset key HolbertonSchools Seattle (integer) 80 
+hset key HolbertonSchools New York (integer) 20 
+hset key HolbertonSchools Bogoda (integer) 20 
+hset key HolbertonSchools Cali (integer) 40 
+hset key HolbertonSchools Paris (integer) 2
+hgetall HolbertonSchools
+
